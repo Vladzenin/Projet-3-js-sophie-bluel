6f76c0f3e0ccbadbdcce8205d130931c0fdbe19d
julien ferté <julienfertenissan@gmail.com>
2024-05-02 13:26:35 +0200

    js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+let score = 0
+
+let motUtilisateur = prompt("Entrez le mot: " + listeMots[0])
+
+if (motUtilisateur === listeMots[0]) {
+    score++
+}
+
+console.log(score);
