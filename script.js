0a290c64a33fba4905882a30f1942b406de1f77c
julien ferté <julienfertenissan@gmail.com>
2024-05-02 13:46:07 +0200

    boucle for js

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
 const listeMots = ["Cachalot", "Pétunia", "Serviette"]
 let score = 0
 
-let motUtilisateur = prompt("Entrez le mot: " + listeMots[0])
-
-if (motUtilisateur === listeMots[0]) {
+for(let i =0; i < listeMots.length; i++) {
+    let motUtilisateur = prompt("Entrez le mot: " + listeMots[i])
+if (motUtilisateur === listeMots[i]) {
     score++
 }
-
-motUtilisateur = prompt("Entrez le mot: " + listeMots[1])
-
-if (motUtilisateur === listeMots[1]) {
-    score++
 }
 
-motUtilisateur = prompt("Entrez le mot: " + listeMots[2])
 
-if (motUtilisateur === listeMots[2]) {
-    score++
-}
-
-console.log(score);
+console.log("Votre score est de " + score + "sur 3");
