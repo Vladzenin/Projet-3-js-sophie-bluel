76961d47116908a4a0ac2968a24df2000d4a4377
julien ferté <julienfertenissan@gmail.com>
2024-05-02 13:57:58 +0200

    prompt js

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"]
 let score = 0
 
-for(let i =0; i < listeMots.length; i++) {
-    let motUtilisateur = prompt("Entrez le mot: " + listeMots[i])
-if (motUtilisateur === listeMots[i]) {
-    score++
+
+let choix = prompt("Veillez choisir la liste : mots ou phrases")
+while (choix !== "mots" && choix !== "phrases") {
+    choix = prompt("Veillez choisir la liste : mots ou phrases")
 }
+
+if (choix === "mots") {
+    for(let i =0; i < listeMots.length; i++) {
+        let motUtilisateur = prompt("Entrez le mot: " + listeMots[i])
+    if (motUtilisateur === listeMots[i]) {
+        score++
+    }
+    }
+    console.log("Votre score est de " + score + "sur " + listeMots.length);
+} else {
+    for(let i =0; i < listePhrases.length; i++) {
+            let motUtilisateur = prompt("Entrez le mot: " + listePhrases[i])
+        if (motUtilisateur === listePhrases[i]) {
+            score++
+        }
+        }
+        console.log("Votre score est de " + score + "sur " + listePhrases.length);
 }
 
 
-console.log("Votre score est de " + score + "sur " + listeMots.length);
+
+
